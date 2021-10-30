7b1f92905b2c6b7280988a82e3f387a3ae07f6e8
Saad Baig <saadbaig7777@gmail.com>
2021-10-30 20:10:14 +0500

    created dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,39 +3,40 @@ import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.mainContainer}>
       <h1>Dashboard</h1>
+      <div className={styles.container}>
+        <div className={styles.leftContainer}>
+          <h2>Today's High Score</h2>
+          <div className={styles.card}>
+            <div className={styles.col}>
+              <p className={styles.title}>Rank</p>
+              <p>1</p>
+              <p>2</p>
+              <p>3</p>
+            </div>
+            <div className={styles.col}>
+              <p className={styles.title}>Name</p>
+              <p>ABC</p>
+              <p>ABC</p>
+              <p>ABC</p>
+            </div>
+            <div className={styles.col}>
+              <p className={styles.title}>Point</p>
+              <p>ABC</p>
+              <p>ABC</p>
+              <p>ABC</p>
+            </div>
+          </div>
 
-      <div className={styles.leftContainer}>
-        <h2>Today's High Score</h2>
-        <div className={styles.card}>
-          <div className={styles.rank}>
-            <p className={styles.title}>Rank</p>
-            <p>1</p>
-            <p>2</p>
-            <p>3</p>
-          </div>
-          <div className={styles.name}>
-            <p className={styles.title}>Name</p>
-            <p>ABC</p>
-            <p>ABC</p>
-            <p>ABC</p>
-          </div>
-          <div className={styles.point}>
-            <p className={styles.title}>Point</p>
-            <p>ABC</p>
-            <p>ABC</p>
-            <p>ABC</p>
-          </div>
+          <p className={styles.yourScore}>Your Score: &nbsp; 21</p>
+          <p className={styles.yourRank}>Your Rank: &nbsp; 42</p>
         </div>
 
-        <p className={styles.yourScore}>Your Score: 21</p>
-        <p className={styles.yourRank}>Your Rank: 42</p>
-      </div>
-
-      <div className={styles.rightContainer}>
-        <h2>All Time High Score</h2>
-        <div className={styles.card}></div>
+        <div className={styles.rightContainer}>
+          <h2>All Time High Score</h2>
+          <div className={styles.card}></div>
+        </div>
       </div>
     </div>
   );
